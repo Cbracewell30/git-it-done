ec0a18736efc52b64bffef79b48e40d36ccfbd94
Chris <chrisbracewell35@gmail.com>
2022-01-06 23:28:02 -0500

    adding second HTML and Java info

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,7 @@
 var issuecontainerEL = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,9 +12,16 @@ var getRepoIssues = function(repo) {
                     response.json().then(function(data){
                         // pass response data to dom function
                         displayIssues(data);
+                
+
+                         // check if api has paginated issues
+    if (response.headers.get("Link")) {
+        displayWarning(repo);
+      }
                     });
                 } else {
-                    alert("There was a problem with your request!");
+                    document.location.replace("./index.html");
+
                 }
             })
 };
@@ -55,4 +65,34 @@ issuecontainerEL.appendChild(issueEl);
 }
 };
 
-getRepoIssues("facebook/react");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  
+    // create link element
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
+
+  var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
+
+  getRepoName();
